1a62f571d7fd697ad176ac7e9527c88b596bcd96
Etienne Canuel <etienne.canuel@gmail.com>
2013-02-17 23:03:16 +0100

    Premier depot

---
+++ Header.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace DXFLibrary
+{
+	/// <summary>
+	/// The representation of DXF header section
+	/// </summary>
+	public class Header : Section
+	{
+		internal Header() : base("HEADER")
+		{
+		}
+		public int VariableCount()
+		{
+			return this.elements.Count;
+		}
+		public Variable getVariable(int index)
+		{
+			return (Variable)this.elements[index];
+		}
+		public object valueOf(string varName)
+		{
+			foreach(Variable v in this.elements)
+			{
+				if(v.VarName == varName) return v.Value;
+			}
+			return null;
+		}
+		public void addVariable(Variable v)
+		{
+			this.elements.Add(v);
+		}
+	}
+}
